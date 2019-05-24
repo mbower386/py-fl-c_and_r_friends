ba2ead7a1d618c8fac59020a6ca7d5ffe6c08166
mbower386 <mbower386@gmail.com>
2019-05-23 20:05:00 -0700

    finished:

--- c_and_r_friends/server.py
+++ c_and_r_friends/server.py
@@ -1,12 +1,30 @@
-from flask import Flask, render_template
-from mysqlconnection import connectToMySQL    # import the function that will return an instance of a connection
+from flask import Flask, render_template, request, redirect
+# import the function that will return an instance of a connection
+from mysqlconnection import connectToMySQL
 app = Flask(__name__)
+
+
 @app.route("/")
 def index():
-    mysql = connectToMySQL('first_flask')	        # call the function, passing in the name of our db
-    friends = mysql.query_db('SELECT * FROM friends;')  # call the query_db function, pass in the query as a string
-    print(friends)
-    return render_template("index.html")
-            
+    # call the function, passing in the name of our db
+    mysql = connectToMySQL('petdb')
+    # call the query_db function, pass in the query as a string
+    pets = mysql.query_db('SELECT * FROM petdb.pets;')
+    print(pets)
+    return render_template("index.html", all_pets=pets)
+
+
+@app.route('/create_pet', methods=["POST"])
+def add_pet_to_db():
+    mysql = connectToMySQL("petdb")
+    query = "INSERT INTO pets (name, type) VALUES (%(pn)s, %(pt)s);"
+    data = {
+        'pn': request.form["p_name"],
+        'pt': request.form["p_type"]
+    }
+    mysql.query_db(query, data)
+    return redirect("/")
+
+
 if __name__ == "__main__":
     app.run(debug=True)
